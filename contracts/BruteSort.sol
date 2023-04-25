7d87b17ba7b3536bc179dbe8c5b09b75a9fb170f
axatbhardwaj <axatbhardwaj@outlook.com>
2023-04-25 11:06:54 +0530

    added brute Sort

---
+++ contracts/BruteSort.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Brutesort {
+    //uint256[] a = [120112,555,33,1,404,55,777,99,928];
+
+    function swap(
+        uint256 A,
+        uint256 B
+    ) internal pure returns (uint256, uint256) {
+        A = A + B;
+        B = A - B;
+        A = A - B;
+        return (A, B);
+    }
+
+    function sort(
+        uint256[] memory data
+    ) public pure returns (uint256[] memory) {
+        uint256[] memory dataStg = data;
+        uint256 len = dataStg.length;
+        for (uint256 i = 1; i < len; i++) {
+            for (uint256 j = 1; j < len; j++)
+                if (dataStg[j] < dataStg[i]) {
+                    (dataStg[j], dataStg[i]) = swap(dataStg[j], dataStg[i]);
+                }
+        }
+        return dataStg;
+    }
+}
